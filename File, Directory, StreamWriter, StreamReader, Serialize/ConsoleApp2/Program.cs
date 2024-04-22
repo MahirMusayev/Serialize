e1621b0ce858c8b058ac30fbd670ad35ed1845ba
Mahir Musayev <146106439+musayevmahir68@users.noreply.github.com>
2024-04-22 23:45:45 +0400

    22 aprel

---
+++ File, Directory, StreamWriter, StreamReader, Serialize/ConsoleApp2/Program.cs
@@ -0,0 +1,84 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            List<string> names = new List<string> { "mahir", "fuat","samir" };
+            Serialize(names);
+            Add("inara");
+            Add("kenan");
+            Delete("samir");
+            Search("mahir");
+            
+
+        }
+        public static void Serialize(List<string> names)
+        {
+            string result = JsonConvert.SerializeObject(names);
+
+            using (StreamWriter sw = new(@"C:\Users\Heydar\Desktop\ConsoleApp2\ConsoleApp2\files\names.json"))
+            {
+                sw.Write(result);
+            }
+        }
+        public static List<string> Deserialize()
+        {
+            string result;
+            using (StreamReader sr = new StreamReader(@"C:\Users\Heydar\Desktop\ConsoleApp2\ConsoleApp2\files\names.json"))
+            {
+                result = sr.ReadToEnd();
+            }
+            Console.WriteLine(result);
+            return JsonConvert.DeserializeObject<List<string>>(result);
+        }
+        public static void Add(string name)
+        {
+
+            List<string> names = Deserialize();
+            names.Add(name);
+            Serialize(names);
+            Console.WriteLine($"{name} lave edildi");
+        }
+        public static bool Search(string name)
+        {
+          
+            List<string> names = Deserialize();
+            bool exists = names.Any(x => x.Contains(name));
+            if (exists)
+            {
+                Console.WriteLine($"{name} siyahida var");
+            }
+            else
+            {
+                Console.WriteLine($"{name} siyahida yoxdu");
+            }
+            return exists;
+
+        }
+        public static void Delete(string name)
+        {
+            List<string> names = Deserialize();
+
+            if (names.Contains(name))
+            {
+                names.Remove(name);
+                Serialize(names);
+                Console.WriteLine($"{name} siyahidan silindi");
+            }
+            else
+            {
+                Console.WriteLine($"{name} siyahida tapilmadi");
+            }
+        }
+      
+
+    }
+
+}
+
---
+++ sinif 22 aprel/ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Category category = new Category { Name = "Mahir" };
+            //Product product = new Product { Id = 1, Name = "IPhone", Price = 2000, Category = category };
+            //Product product1 = new Product { Id = 2, Name = "Samsung", Price = 1000, Category = category };
+            //Product product2 = new Product { Id = 3, Name = "Xiami 15", Price = 200, Category = category };
+
+            //List<Product> products = new List<Product> { product, product1, product2 };
+            //string json = JsonConvert.SerializeObject(products);
+            //using (StreamWriter sw = new StreamWriter(@"C:\Users\Heydar\Desktop\ConsoleApp1\ConsoleApp1\Products.json"))
+            //{
+            //    sw.Write(json);
+            //}
+
+
+            string result;
+            using (StreamReader sr = new StreamReader(@"C:\Users\Heydar\Desktop\ConsoleApp1\ConsoleApp1\Products.json"))
+            {
+                result = sr.ReadToEnd();
+            }
+            Console.WriteLine(result);
+
+
+
+
+        }
+    }
+    internal class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public Category Category { get; set; }
+    }
+    internal class Category
+    {
+        public string Name { get; set; }
+    }
+}
